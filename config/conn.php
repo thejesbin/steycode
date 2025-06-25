cee526a5feecdcd54cc3ac932203fc9ea1d124b5
Jesbin <jesbin@courtclick.com>
2025-06-25 10:15:06 +0530

    chore: update database connection credentials for production environment

--- config/conn.php
+++ config/conn.php
@@ -1,7 +1,7 @@
 <?php
 // Create connection
-$conn =  mysqli_connect('127.0.0.1', 'root', '', 'steycode');
-
+// $conn =  mysqli_connect('127.0.0.1', 'root', '', 'steycode');
+$conn =  mysqli_connect('127.0.0.1', 'herrtxid_root', 'Bahubali@1', 'herrtxid_steycode');
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
